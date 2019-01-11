746613e9caa500a505fddc05b0bf095a74cca223
cclark25 <clark-chris@comcast.net>
2019-01-10 20:11:29 -0500

    Modified movement to only follow mouse when targeting. Created default movement with standard WASD movement. created initial jump mechanics.

--- icon.gd
+++ icon.gd
@@ -4,7 +4,11 @@ extends Sprite
 # var a = 2
 # var b = "textvar"
 var velocity = Vector2();
-export (int) var speed = 10;
+export (int) var speed = 5;
+var targeting = false;
+var jumpTime = .25;
+var inJump = false;
+var forward = 0;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -41,13 +45,60 @@ func get_input(delta):
 	var rotOffset = Vector2();
 	rotOffset.y = sin(newAngle);
 	rotOffset.x = cos(newAngle);
-	printerr(newAngle);
 	if(x != 0):
 		position = get_viewport().get_mouse_position() + (rotOffset.normalized() * cursorAngle.length());
-	rotation = newAngle;
 	
+	velocity = get_viewport().get_mouse_position() - position;
+	
+	
+func move_normal(delta):
+	var newVelocity = Vector2();
+	if Input.is_action_pressed('Up'):
+        newVelocity.y -= 1
+	if Input.is_action_pressed('Down'):
+        newVelocity.y += 1
+	if Input.is_action_pressed('Left'):
+        newVelocity.x -= 1
+	if Input.is_action_pressed('Right'):
+        newVelocity.x += 1
+	if (newVelocity.length() != 0):
+		velocity = newVelocity.normalized() * speed;
+		position += velocity;
+
+func jump(delta):
+	if !inJump:
+		inJump = true;
+		forward = !Input.is_action_pressed("Down");
+	
+	if targeting:
+		var x = 1;
+		if !forward:
+			x = -1;
+		printerr(x);
+		position += velocity.normalized() * speed * (1 + sin(PI * jumpTime/0.25)) * x;
+	else:
+		position += velocity.normalized() * speed * (1 + sin(PI * jumpTime/0.25));
+	
+	jumpTime -= delta;
+	if jumpTime <= 0:
+		inJump = false;
+		jumpTime = .25;
+
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	get_input(delta);
+	if Input.is_action_just_pressed("Target"):
+		targeting = !targeting;
+	
+	if Input.is_action_just_pressed("Jump") || inJump:
+		jump(delta);
+		return;
+	
+	if targeting: 
+		get_input(delta);
+	else:
+		move_normal(delta);
+	
+	rotation = velocity.angle();
+	
 	pass
