d4c713a5292d550b9379ce3369995eebdc7af14f
Morgan Channell <merbertmo@gmail.com>
2019-02-25 10:48:32 -0500

    [Morgan] Dryad Aggros when player is close

--- Sword.gd
+++ Sword.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Sprite
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,14 +7,7 @@ extends Area2D
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	connect("area_entered", self, "DealDamage");
 	pass
-
-func DealDamage(body):
-	if (body.get_parent() == body.get_parent().get_parent().get_child(0)):
-		body.get_parent().Damage(4, get_parent().position);
-	pass;
-
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
