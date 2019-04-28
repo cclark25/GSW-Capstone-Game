7b5108609cc1398fff34ec3c241fd3fa993ac2ca
cclark25 <clark-chris@comcast.net>
2019-04-28 19:55:11 -0400

    Added enemies to individual rooms to demonstrate them.

---
+++ Scenes/Rooms/H1/H1.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func handleDeath(body):
+	if(body == get_node("DryadRangerBody")):
+		printerr("Dryad Killed!");
+		#load("res://Scenes/Entities/Ite");
+	return;
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
